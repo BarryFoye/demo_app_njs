c2bad4ab0d475290f5efdf2fac39cacb40c31b42
BarryFoye <barry.foye@gmail.com>
2021-09-08 11:33:22 +0100

    added a timeout for the npm install to complete. bad workaround

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
-start /wait npm install
+start npm install
+ECHO Waiting 30 seconds for npm install, if it completes sooner:
+timeout /t 30
 start node hidden_server/index.js
 timeout /t 1
 start node gateway_server/index.js
